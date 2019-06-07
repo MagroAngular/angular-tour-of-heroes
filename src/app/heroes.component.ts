6df3800f91bea77d7ccef2d5f465944394c07c6f
amagrpfe <amagrpfe@everis.com>
2019-06-07 13:43:02 +0200

    c2

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -31,6 +31,11 @@ export class HeroesComponent implements OnInit {
     this.addingHero = true;
     this.selectedHero = null;
   }
+  
+  addHero2(): void {
+    this.addingHero = true;
+    this.selectedHero = null;
+  }
 
   close(savedHero: Hero): void {
     this.addingHero = false;
