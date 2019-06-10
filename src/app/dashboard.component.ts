e896a2d05fbef659c198d865bf6196e05df4f619
amagrpfe <amagrpfe@everis.com>
2019-06-10 12:31:23 +0200

    eliminar variables

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -11,7 +11,6 @@ import { HeroService } from './hero.service';
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
-  heroes1: Hero[] = [];
   constructor(
     private router: Router,
     private heroService: HeroService) {
